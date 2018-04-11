7621ae96f73663d940b7647495d0aceb9d6c5dda
Romain Pelisse <belaran@gmail.com>
2018-04-11 16:40:06 +0200

    Lab7 - Security Manager

--- danslecambouis/src/main/java/chez/les/barbus/danslecambouis/DanslecambouisApplication.java
+++ danslecambouis/src/main/java/chez/les/barbus/danslecambouis/DanslecambouisApplication.java
@@ -2,17 +2,16 @@ package chez.les.barbus.danslecambouis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @ComponentScan("chez.les.barbus.danslecambouis")
 @SpringBootApplication
 public class DanslecambouisApplication {
 
 	public static void main(String[] args) {
+		if ( System.getSecurityManager() == null )
+			System.setSecurityManager(new PingSecurityManager());
 		SpringApplication.run(DanslecambouisApplication.class, args);
 	}
+
 }

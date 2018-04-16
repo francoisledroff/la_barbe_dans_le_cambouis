b7c80b9369faf95ad07200de76f6cf5f44dfe175
ledroff <ledroff@adobe.com>
2018-04-16 15:44:13 +0200

    adding sso with github oauth authentication

--- danslecambouis/src/main/java/chez/les/barbus/danslecambouis/rest/AccountController.java
+++ danslecambouis/src/main/java/chez/les/barbus/danslecambouis/rest/AccountController.java
@@ -1,6 +1,8 @@
 package chez.les.barbus.danslecambouis.rest;
 
 import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,13 @@ public class AccountController {
   @GetMapping()
   public String getAccount(Principal principal) {
     return "TODO account for principal "+principal.getName();
+  }
+
+  @GetMapping( "/me" )
+  public Map<String, String> user(Principal principal) {
+    Map<String, String> map = new LinkedHashMap<>();
+    map.put("name", principal.getName());
+    return map;
   }
 
 }

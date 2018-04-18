a798c5a3f3acaa5d84a24228f89e30b39f76e640
Romain Pelisse <belaran@gmail.com>
2018-04-18 14:56:41 +0200

    Modif /ping lab selinux

---
+++ danslecambouis/src/main/docker/selinux-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dnf install -y policycoreutils policycoreutils-python selinux-policy selinux-policy-targeted libselinux-utils setroubleshoot-server setools setools-console mcstrans
--- danslecambouis/src/main/java/chez/les/barbus/danslecambouis/rest/PingController.java
+++ danslecambouis/src/main/java/chez/les/barbus/danslecambouis/rest/PingController.java
@@ -1,5 +1,11 @@
 package chez.les.barbus.danslecambouis.rest;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/ping")
 public class PingController {
 
-  @GetMapping()
-  public String ping() {
-    return "pong";
-  }
+	private static final Path LOG_FILE = Paths.get("/var/log/ping.log");
+
+	@GetMapping()
+	public String ping() {
+		try {
+			Files.write(Paths.get("/var/log/ping.log"), ("Called!").getBytes(),
+					StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			System.out.println("Can't log :/");
+			e.printStackTrace();
+		}
+		return "pong";
+	}
 
 }
